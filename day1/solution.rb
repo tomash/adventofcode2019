a57832b145d2a25ffb5224bbe025411d4c91e7d7
Tomash <tomekrs@o2.pl>
2019-12-01 19:41:42 +0100

    day 1, part 2 done -- day 1 done

--- day1/solution.rb
+++ day1/solution.rb
@@ -3,8 +3,24 @@ def fuel_required_for_module(mass)
   fuelreq
 end
 
+def fuel_required_for_module_part2(mass)
+  last = fuel_required_for_module(mass)
+  total = 0
+  while(last > 0) do
+    total += last
+    last = fuel_required_for_module(last)
+  end
+  total
+end
+
+# Part One
 masses = File.open("input.txt", "r").readlines.map(&:to_i)
 fuels = masses.map{ |m| fuel_required_for_module(m) }
 total_fuel = fuels.sum
+puts "Part One: total fuel required: #{total_fuel}"
 
-puts "total fuel required: #{total_fuel}"
+# Part Two:
+
+fuels_two = masses.map{ |m| fuel_required_for_module_part2(m) }
+total_fuel_two = fuels_two.sum
+puts "Part Two: total fuel required: #{total_fuel_two}"
